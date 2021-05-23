f573a9e65c2f2c409153fef1be292dd74bb77822
Lucas Cruz <lucascs20182@gmail.com>
2021-05-23 10:23:25 -0300

    corrija modo dark

--- scripts/dark-mode-customized.js
+++ scripts/dark-mode-customized.js
@@ -1,45 +1,41 @@
 function personalizaComponentesNaoEstilizadosPeloPlugin() {
-    const iptDarkSwitch = document.querySelector('#confereDark');
-    // const navBar = document.querySelector('#navbar-switcher');
-    // const card = document.querySelectorAll('.card');
-    // const textCard = document.querySelectorAll('div.cards p');
-    // const imgLogo = document.querySelector('#navbar-switcher > div > a > div > img');
+    const iptDarkSwitch = document.querySelector('#darkSwitch');
+    const navBar = document.querySelector('#navbar-switcher');
+    const card = document.querySelectorAll('.card');
+    const textCard = document.querySelectorAll('div.cards p');
+    const imgLogo = document.querySelector('#navbar-switcher > div > a > div > img');
 
-    if(iptDarkSwitch.value.equals == "true"){
-        document.body.setAttribute("data-theme","dark");
-        localStorage.setItem("darkSwitch","dark");
-        // iptDarkSwitch.value = "true";
+    if(!localStorage.getItem("darkSwitch")!==null&&localStorage.getItem("darkSwitch")==="dark"){
+        // console.log(iptDarkSwitch.checked)
 
-        // navBar.classList.remove("navbar-light");
-        // navBar.classList.add("navbar-dark");
+        navBar.classList.remove("navbar-light");
+        navBar.classList.add("navbar-dark");
 
-        // card[0].style.backgroundColor = "#111";
-        // card[1].style.backgroundColor = "#111";
-        // card[2].style.backgroundColor = "#111";
+        card[0].style.backgroundColor = "#111";
+        card[1].style.backgroundColor = "#111";
+        card[2].style.backgroundColor = "#111";
 
-        // textCard[0].style.color = "#fff";
-        // textCard[1].style.color = "#fff";
-        // textCard[2].style.color = "#fff";
+        textCard[0].style.color = "#fff";
+        textCard[1].style.color = "#fff";
+        textCard[2].style.color = "#fff";
 
-        // imgLogo.src="./assets/logo-dark.png";
+        imgLogo.src="./assets/logo-dark.png";
 
     }else{
-        document.body.removeAttribute("data-theme");
-        localStorage.removeItem("darkSwitch");
-        // iptDarkSwitch.value = "false";
+        // console.log(iptDarkSwitch.checked)
 
-        // navBar.classList.remove("navbar-dark");
-        // navBar.classList.add("navbar-light");
+        navBar.classList.remove("navbar-dark");
+        navBar.classList.add("navbar-light");
 
-        // card[0].style.backgroundColor = "#fff";
-        // card[1].style.backgroundColor = "#fff";
-        // card[2].style.backgroundColor = "#fff";
+        card[0].style.backgroundColor = "#fff";
+        card[1].style.backgroundColor = "#fff";
+        card[2].style.backgroundColor = "#fff";
 
-        // textCard[0].style.color = "#000";
-        // textCard[1].style.color = "#000";
-        // textCard[2].style.color = "#000";
+        textCard[0].style.color = "#000";
+        textCard[1].style.color = "#000";
+        textCard[2].style.color = "#000";
 
-        // imgLogo.src="./assets/logo.png";
+        imgLogo.src="./assets/logo.png";
     }
 };
 
